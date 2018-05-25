201c04a63551c25ade95734506c625104c0c8ca3
Sean Vincent <sean@searchspring.com>
2018-05-25 09:48:01 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.sean-vincent/clj-typesafe "0.1.0-SNAPSHOT"
+  :description "Simple wrapper around Lightbend Typesafe config library."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.typesafe/config "1.3.2"]])
---
+++ test/clj_typesafe/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-typesafe.core-test
+  (:require [clojure.test :refer :all]
+            [clj-typesafe.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
