fbe753cf1cd06c53d30a2a9017d2334633b5f83b
Sean Vincent <sean@searchspring.com>
2018-05-25 10:35:56 -0600

    Update project URL and version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.sean-vincent/clj-typesafe "0.1.0-SNAPSHOT"
+(defproject org.clojars.sean-vincent/clj-typesafe "0.1.0"
   :description "Simple wrapper around Lightbend Typesafe config library."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/svincen2/clj-typesafe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
